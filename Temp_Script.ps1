6d80cedfd2c982fd68f35b912033310fac732a0f
Sidrisu <85908711+Sidrisu@users.noreply.github.com>
2021-06-14 19:01:22 -0700

    Update Temp_Script.ps1

--- Temp_Script.ps1
+++ Temp_Script.ps1
@@ -1,36 +1,37 @@
 $unit = Read-host -Prompt 'Please provide the unit of measure (C, K, or F)'
 if (($unit -eq 'C') -or ($unit -eq 'K') -or ($unit -eq 'F')) {
     $value = Read-Host -Prompt 'Please provide the measured value'
+    Try{
+        [decimal]$value | Out-Null
+    } Catch {
+        Write-Host 'Value entered was not in a number format.'
+        break
+    }
 }
 else {
     Write-Host 'Unsupported unit of measure entered.'
-break
+    break
 }
-$temptable = @(
-    [PSCustomObject]@{'Kelvin' = $valuek}
-    [PSCustomObject]@{'Farhenheit' = $valuef}
-    [PSCustomObject]@{'Celsius' = $valuec}
-)
-if ($unit -eq 'C') {
-    $valuec = $value;
-    $valuek = ($value + 273.15);
-    $valuef = ($value * (9/5) + 32)
-    write-host $temptable | Format-Table
+switch ($unit) {
+    'C' { 
+        $valuec = "{0:####.00}" -f [decimal]$value
+        $valuek = "{0:####.00}" -f ([decimal]$value + 273.15)
+        $valuef = "{0:####.00}" -f ([decimal]$value * (9/5) + 32)
+        Write-Host 'Kelvin:' $valuek ' Farhenheit:' $valuef ' Celsius' $valuec
+     }
+    'K' { 
+        $valuek = "{0:####.00}" -f [decimal]$value
+        $valuec = "{0:####.00}" -f ([decimal]$value - 273.15)
+        $valuef = "{0:####.00}" -f (([decimal]$value - 273.15) * (9/5) + 32)
+        Write-Host 'Kelvin:' $valuek ' Farhenheit:' $valuef ' Celsius' $valuec
+     }
+    'F' { 
+        $valuef = "{0:####.00}" -f [decimal]$value
+        $valuec = "{0:####.00}" -f (([decimal]$value - 32) * (5/9))
+        $valuek = "{0:####.00}" -f (([decimal]$value - 32) * (5/9) + 273.15)
+        Write-Host 'Kelvin:' $valuek ' Farhenheit:' $valuef ' Celsius' $valuec
+     }
 }
-elseif ($unit -eq 'K') {
-    write-host $tempheadings
-    $valuek = $value;
-    $valuec = ($value - 273.15);
-    $valuef = (($value -273.5) * (9/5) + 32)
-    $tempvalues = @($valuek,$valuef,$valuec)
-    write-host $tempvalues
-}
-else {
-    write-host $tempheadings
-    $valuef = $value;
-    $valuec = ($value - 32) *  (5/9);
-    $valuek = (($value - 32) * (5/9) + 273.15)
-    $tempvalues = @($valuek,$valuef,$valuec)
-    write-host $tempvalues
 
-}
+
+
